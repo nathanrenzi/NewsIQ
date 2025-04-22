57e4069baee5e449002200018ed2c89bd85a445f
mejiad20g <165347752+mejiad20g@users.noreply.github.com>
2025-04-22 19:14:58 -0400

    Delete NewsIQ-daniel-test directory

--- NewsIQ-daniel-test/client/src/pages/QuizGenerator/Answer.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default function Answer({type, answer, onClick}) {
-    return (
-        <button className={["answerButton", type].join(" ")} onClick={onClick}>{answer}</button>
-    )
-}
