f7b03e9582036f104f929b226f1d91274a3ac20c
jxsir03 <142930242+jxsir03@users.noreply.github.com>
2025-04-21 21:55:57 -0400

    Homepage updated

--- client/src/pages/Homepage/Homepage.js
+++ client/src/pages/Homepage/Homepage.js
@@ -1,7 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import "./Homepage.css";
 
 export default function Homepage() {
-    return (
-        <p>Homepage!</p>
-    )
+  return (
+    <>
+      <header className="top-bar">
+        <span className="logo">NewsIQ</span>
+      </header>
+
+      <div className="homepage-container">
+        <h1 className="headline">
+          <span>Log in or make an account</span>
+          <br />
+          <span>to access NewsIQ!</span>
+        </h1>
+
+        <div className="auth-container">
+          <Link to="/login" className="auth-box">
+            Log In
+          </Link>
+          <div className="auth-or">Or</div>
+          <Link to="/signup" className="auth-box">
+            Sign Up
+          </Link>
+        </div>
+      </div>
+    </>
+  );
 }
